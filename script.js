a514f2528eb73f7e1d85a560ff3c3d0aead85ae1
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-23 17:20:29 +0300

    feat: visitors managment

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function AddValue(key, value) {
 }
 
 function DeleteValue(key) {
-  data[key] = undefined;
+  delete data[key];
 }
 
 function GetValueInfo(key) {
@@ -13,8 +13,93 @@ function GetValueInfo(key) {
 }
 
 function ListValues() {
-  const str = '';
+  let str = '';
+
   for (let key in data) {
     str += `${key} - ${data[key]}\n`;
   }
+
+  return str;
+}
+
+const actionCancelledStr = 'Action cancelled';
+
+function PromptAddVisitor() {
+  const name = prompt('The name of the visitor?');
+
+  if (!name) {
+    alert(actionCancelledStr);
+    return;
+  }
+
+  const login = prompt('Login?');
+
+  if (!login) {
+    alert(actionCancelledStr);
+    return;
+  }
+
+  if (data[login]) {
+    const override = confirm('This login already exists. Would you like to override the existing name?');
+
+    if (!override) {
+      alert(actionCancelledStr);
+      return;
+    }
+  }
+
+  alert('Sucessful!');
+
+  AddValue(login, name);
+}
+
+function PromptDeleteVisitor() {
+  const login = prompt('Login of the deleted visitor?');
+
+  if (!login) {
+    alert(actionCancelledStr);
+    return;
+  }
+
+  if (data[login]) {
+    const confirmed = confirm('Are you sure you want to delete this visitor?');
+
+    if (!confirmed) {
+      alert(actionCancelledStr);
+      return;
+    }
+
+    DeleteValue(login);
+    alert('Sucessful!');
+    return;
+  }
+
+  alert('Error: there is no such visitor');
+}
+
+function AlertVisitorName() {
+  const login = prompt('Login of the visitor?');
+
+  if (!login) {
+    alert(actionCancelledStr);
+    return;
+  }
+
+  if (data[login]) {
+    alert(GetValueInfo(login));
+    return;
+  }
+
+  alert('Error: there is no such visitor');
+}
+
+function LogAllVisitors() {
+  const log = ListValues();
+
+  if (log.length === 0) {
+    console.log('No registered visitors yet');
+    return;
+  }
+
+  console.log(log);
 }
