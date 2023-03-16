83b30b66c3024cb25179a083368db27fbb22fd0d
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-03-16 18:15:27 +0300

    feat: form validation

--- Form/script.js
+++ Form/script.js
@@ -1,137 +1,131 @@
+// -------------------
+// Inputs Creation
+// -------------------
+
+const setFields = (fields, formName) => {
+  const form = document.forms.namedItem(formName);
+  for (const field of fields) {
+    const elem = fieldCreateElement(field, form);
+    form.appendChild(elem);
+  }
+}
+
 const formFields = [
   {
     label: 'Enter your name:',
     attributes: {
       type: 'text',
       name: 'name',
+      className: 'review_name',
       required: true,
     },
-    class: 'name',
   },
   {
     label: 'Enter your email:',
     attributes: {
       type: 'email',
       name: 'email',
+      className: 'review_email',
       required: true
     },
-    class: 'email',
   },
   {
     label: 'Enter rating:',
     attributes: {
       type: 'number',
       name: 'rating',
+      className: 'review_rating',
       required: true,
     },
-    class: 'rating',
-    min: 1,
-    max: 10,
-    validator: (value) => {
-      if (!Number.isInteger(value)) {
-        return errorType(name, 'integer');
-      }
-      if (value < min || value > max) {
-        return errorRange(name, min, max);
-      }
-      return null;
-    }
   },
   {
     fieldset: {
       legend: 'Agree with terms and conditions',
-      name: 'terms',
-      class: 'terms',
+      className: 'review__terms',
       fields: [
         {
           label: 'Yes',
           attributes: {
             type: 'radio',
+            className: 'review__terms_radio',
+            name: 'terms',
             id: 'yes',
             value: true,
           },
-          class: 'radio'
         },
         {
           label: 'No',
           attributes: {
             type: 'radio',
+            className: 'review__terms_radio',
+            name: 'terms',
             id: 'no',
             value: false,
           },
-          class: 'radio'
         }
       ]
     }
-    // validator: (value) => {
-    //   if (value) {
-    //     return null;
-    //   }
-    //   return 'you have to agree with terms and conditions';
-    // }
   },
   {
     attributes: {
       type: 'submit',
+      className: 'review_submit',
       value: 'Send'
     }
   }
 ];
 
-const errorEmpty = (
-  name
-) => `${name} field is empty`;
-
-const errorType = (
-  name, type
-) => `${name} should be ${type}`;
-
-const errorRange = (
-  name, min, max
-) => `${name} should be in range from ${min} to ${max}`;
-
-const setFields = (fields, form, container = form) => {
-  for (const field of fields) {
-    if (field.label) {
-      const label = fieldCreateLabel(field);
-      container.appendChild(label);
-    }
-
-    if (field.attributes) {
-      const elem = fieldCreateInput(field, form);
-      container.appendChild(elem);
-    }
-
-    if (field.fieldset) {
-      const elem = fieldCreateFieldset(field.fieldset, form);
-      container.appendChild(elem);
-    }
+const fieldCreateElement = (field, form) => {
+  if (field.fieldset) {
+    return fieldCreateFieldset(field.fieldset, form);
   }
+
+  let elem;
+
+  if (field.label) {
+    elem = fieldCreateLabel(field);
+  }
+
+  if (field.attributes) {
+    const input = fieldCreateInput(field);
+
+    if (!elem) {
+      return input;
+    }
+
+    elem.appendChild(input);
+  }
+
+  elem.form = form;
+
+  return elem;
 }
 
 const fieldCreateFieldset = (fieldset, form) => {
   const elem = document.createElement('fieldset');
 
   if (fieldset.legend) {
-    const legend = document.createElement('legend');
-    legend.innerHTML = fieldset.legend;
+    const legend = fieldsetCreateLegend(fieldset);
     elem.appendChild(legend);
   }
 
-  if (fieldset.class) {
-    const blockClass = form.classList[0];
-    const fieldsetClass = `${blockClass}__${fieldset.class}`;
-    elem.classList.add(fieldsetClass);
-  }
-
-  for (const field of fieldset.fields) {
-    field.attributes.name = fieldset.name;
-  }
-
-  elem.form = form;
-  setFields(fieldset.fields, form, elem);
+  fieldsetCreateFields(fieldset.fields, form, elem);
 
   return elem;
+}
+
+const fieldsetCreateFields = (fields, form, container) => {
+  for (const field of fields) {
+    const elem = fieldCreateElement(field, form);
+    container.appendChild(elem);
+  }
+}
+
+const fieldsetCreateLegend = (fieldset) => {
+  const legend = document.createElement('legend');
+  legend.innerHTML = fieldset.legend;
+
+  return legend;
 }
 
 const fieldCreateInput = (
@@ -139,10 +133,10 @@ const fieldCreateInput = (
 ) => {
   let elem = document.createElement('input');
 
-  if (field.class) {
-    const blockClass = container.classList[0];
-    const elemClass = `${blockClass}_${field.class}`;
-    elem.classList.add(elemClass);
+  if (field.className) {
+    const blockclassName = container.classNameList[0];
+    const elemclassName = `${blockclassName}_${field.className}`;
+    elem.classNameList.add(elemclassName);
   }
 
   elem.form = form;
@@ -188,5 +182,99 @@ const fieldGetFor = (field) => {
   }
 }
 
-const form = document.getElementById('review');
-setFields(formFields, form);
+// -------------------
+// Form Setting
+// -------------------
+
+setFields(formFields, 'review');
+
+// -------------------
+// Validation
+// -------------------
+
+const errorsList = document.querySelector('.errors__list');
+
+const errorCreate = (message) => {
+  const error = document.createElement('li');
+  error.textContent = `Error: ${message}`;
+  error.classList.add('errors__list_item');
+
+  return error;
+}
+
+const cleanErrorsList = () => {
+  while (errorsList.lastElementChild) {
+    errorsList.removeChild(errorsList.lastElementChild);
+  }
+}
+
+const errorEmpty = (
+  name
+) => `${name} field is empty`;
+
+const errorType = (
+  name, type
+) => `${name} should be ${type}`;
+
+const errorRange = (
+  name, min, max
+) => `${name} should be in range from ${min} to ${max}`;
+
+const validateName = (value) => {
+  const regex = /^[a-zA-Z ]*$/;
+  if (!regex.test(value)) {
+    return 'name should only contain latin letters';
+  }
+
+  return null;
+}
+
+const validateRating = (value) => {
+  const min = 1;
+  const max = 10;
+
+  if (value < min || value > max) {
+    return errorRange('rating', min, max);
+  }
+
+  return null;
+}
+
+const validateTerms = (value) => {
+  if (value == 'false') {
+    return 'you have to agree with terms and conditions to submit your review';
+  }
+
+  return null;
+}
+
+const validator = {
+  name: validateName,
+  rating: validateRating,
+  terms: validateTerms
+}
+
+const reviewValidate = () => {
+  const form = document.forms.namedItem('review');
+  const inputs = form.elements;
+
+  let valid = true;
+  cleanErrorsList();
+
+  for (const key in validator) {
+    const input = inputs.namedItem(key);
+
+    const errorMessage = validator[key](input.value);
+
+    if (!errorMessage) {
+      continue;
+    }
+
+    valid = false;
+
+    const error = errorCreate(errorMessage);
+    errorsList.appendChild(error);
+  }
+
+  return valid;
+}
