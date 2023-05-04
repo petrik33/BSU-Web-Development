99eecf6540328c775c84970fae8d73e3bd4afc3b
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-05-04 21:10:32 +0300

    fix: RxJS functionality

--- Lab 8. Reactive Programming with RxJS/src/index.js
+++ Lab 8. Reactive Programming with RxJS/src/index.js
@@ -1,13 +1,29 @@
 import './styles/styles.css';
 import './styles/variables.css';
 
-import { concatMap, fromEvent } from 'rxjs';
-import { switchMap, map } from 'rxjs';
+import { fromEvent, from } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 const showDataButton = document.getElementById('show-data');
 const deleteDataButton = document.getElementById('delete-data');
-const dataContainer = document.getElementById('data-container')
+const dataContainer = document.getElementById('data-container');
+
+const showData$ = fromEvent(showDataButton, 'click');
+showData$.subscribe(() => {
+  from(ajax.getJSON('../data')).subscribe(data => {
+    loadData(data);
+  });
+})
+
+const deleteData$ = fromEvent(deleteDataButton, 'click');
+deleteData$.subscribe(deleteLastDataRow);
+
+const loadData = (data) => {
+  for (const key in data) {
+    const row = loadVisitor(key, data[key]);
+    dataContainer.append(row);
+  }
+}
 
 const loadVisitor = (login, name) => {
   const row = document.createElement('tr');
@@ -20,18 +36,6 @@ const loadVisitor = (login, name) => {
   return row;
 }
 
-const data$ = from(ajax.getJSON('../data'));
-
-const showData$ = fromEvent(showDataButton, 'click').pipe(
-  switchMap(() => data$),
-  concatMap(data => from(data)),
-  map(({ login, name }) => loadVisitor(login, name))
-);
-
-showData$.subscribe(row => {
-  dataContainer.appendChild(row);
-});
-
 const deleteLastDataRow = () => {
   const rows = dataContainer.querySelectorAll('tr');
   if (rows.length > 0) {
@@ -39,6 +43,3 @@ const deleteLastDataRow = () => {
     lastRow.parentNode.removeChild(lastRow);
   }
 };
-
-const deleteData$ = fromEvent(deleteDataButton, 'click');
-deleteData$.subscribe(deleteLastDataRow);
