f2dbe87c355d9e526212c25800db63cf2e0aa7a8
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-05-18 23:16:24 +0300

    feat: toy details

--- Lab 9. Angular and TypeScript/ng-project/src/app/playground/toy-details/toy-details.component.ts
+++ Lab 9. Angular and TypeScript/ng-project/src/app/playground/toy-details/toy-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Toy, Toys } from 'src/data/mock-toy-list';
 
 @Component({
   selector: 'app-toy-details',
   templateUrl: './toy-details.component.html',
   styleUrls: ['./toy-details.component.css']
 })
-export class ToyDetailsComponent {
+export class ToyDetailsComponent implements OnInit {
+  toy: Toy | null = null;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const toyId = this.route.snapshot.paramMap.get('id');
+    // Fetch the toy data based on the toyId
+    // Replace this with your actual code to fetch the toy data
+
+    // Example code to fetch toy data from mock-toy-list
+    const toyData = Toys.find((toy) => toy.id === Number(toyId))
+    if (toyData) {
+      this.toy = toyData;
+    }
+  }
 }
