440295c4987a383b80851626bb79be7d49adacea
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-05-18 23:23:06 +0300

    misc: - custom jumbotron

--- Lab 9. Angular and TypeScript/ng-project/src/app/app.module.ts
+++ Lab 9. Angular and TypeScript/ng-project/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomJumbotronComponent } from './custom-jumbotron/custom-jumbotron.component';
 import { PlaygroundModule } from './playground/playground.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomJumbotronComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
