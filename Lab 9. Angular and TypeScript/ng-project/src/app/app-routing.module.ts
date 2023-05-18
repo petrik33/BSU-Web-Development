95c491f8393e97b2c538cbbeeafb0f648cb56573
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-05-18 23:15:49 +0300

    config: routing

--- Lab 9. Angular and TypeScript/ng-project/src/app/app-routing.module.ts
+++ Lab 9. Angular and TypeScript/ng-project/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ToyCenterComponent } from './playground/toy-center/toy-center.component';
+import { ToyDetailsComponent } from './playground/toy-details/toy-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: ToyCenterComponent },
+  { path: 'playground', redirectTo: '', pathMatch: 'full' },
+  { path: 'playground/toy-details/:id', component: ToyDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
