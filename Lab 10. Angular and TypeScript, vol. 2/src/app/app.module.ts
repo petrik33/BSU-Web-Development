d0b5e833cf38e6dede8fed37c45ac1a8bb5dc2d2
Timofey Petrikevich <petrik3003@gmail.com>
2023-09-09 11:05:06 +0300

    feat: utils module

--- Lab 10. Angular and TypeScript, vol. 2/src/app/app.module.ts
+++ Lab 10. Angular and TypeScript, vol. 2/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UtilsModule } from './utils/utils.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
